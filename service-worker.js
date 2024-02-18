93dd8ed190b8211786aee0a5bdde204b5f7536c0
Garrett-R16 <16garrett.russell@gmail.com>
2024-02-17 19:00:22 -0500

    added function for keeping track of subGroup and mainGroup titles (and updating them), drafted class for creating tabArrays

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
 console.log(chrome.tabGroups);
 
 //Refreshing current list if tab/group orientation has changed
-chrome.tabGroups.onRemoved.addListener(refreshIds);
+chrome.tabGroups.onRemoved.addListener(tabRemoved);
+
+//this isn't working!!!!!!!!!!!!!!!!!!!!!
+chrome.tabGroups.onUpdated.addListener(tabUpdated);
+
+
 chrome.tabs.onCreated.addListener(refreshIds);
 chrome.tabs.onMoved.addListener(refreshIds);
 
@@ -14,12 +19,16 @@ let prevIds = [];
 let groupIds = [];
 let prevGroups = [];
 
+let subGroupIds = [];
+
 refreshIds();
+
+// functions for getting the list of open group ids and tabs
 
 function getTabIds() {
     return new Promise((resolve, reject) => {
         chrome.tabs.query({}, tabs => {
-            tabIds = tabs.map(tab => ({ id: tab.id, groupId: tab.groupId }));
+            tabIds = tabs.map(tab => ({ id: tab.id, groupId: tab.groupId, url: tab.url }));
             resolve(tabIds);
         });
     });
@@ -34,6 +43,8 @@ function getGroupIds() {
     });
 }
 
+//combining the functions into one
+
 function refreshIds() {
     getGroupIds().then(groupIds => {
         console.log(groupIds);
@@ -44,6 +55,45 @@ function refreshIds() {
         console.log(tabIds);
         prevIds = tabIds;
     })
+}
+
+// function for getting the group object assosiate with x id
+
+function getGroupObject(groupId) {
+    groupIds.forEach(groupObj => {
+        if (groupObj.id==groupId) {
+            return groupObj;
+        }
+    })
+    return {};
+}
+
+//this isn't working!!!!!!!!!!!!!!!!!!!!!
+function tabUpdated(group) {
+    subGroupIds.forEach(subGroup => {
+        if (subGroup.subId == group.id && group.title.substring(0,3)!='sub') {
+
+            subGroup.subTitle = group.title;
+
+            chrome.tabGroups.update(group.id, { title: `sub-${subGroup.title} ${subGroup.subTitle}` });
+            return;
+        }
+        if (subGroup.mainId == group.id) {
+            subGroup.title = group.title;
+            chrome.tabGroups.update(subGroup.subId, { title: `sub-${subGroup.title} ${subGroup.subTitle}` });
+        }
+    })
+}
+
+function tabRemoved(group) {
+    for(let i = 0; i < subGroupIds.length; i++) {
+        console.log(subGroupIds[i])
+        if (group.id == subGroupIds[i].subId) {
+            subGroupIds.splice(i, 1);
+            break;
+        }
+    }
+    refreshIds();
 }
 
 //main function for updating the tab groups
@@ -58,7 +108,7 @@ function tabCreated(group) {
             if (tab.groupId==group.id) {
                 console.log("Tab in group", tab.id);
                 //logging new group
-
+                
                 //iterating through array with previous group ids and comparing it to new group for that tab
                 prevIds.forEach(prevTab => {
                     if (prevTab.id==tab.id && prevTab.groupId!=group.id && prevTab.groupId!=-1) {
@@ -68,6 +118,9 @@ function tabCreated(group) {
                         prevGroups.forEach(pGroup => {
                             if (pGroup.id==prevTab.groupId) {
                                 console.log("attempted?")
+
+                                subGroupIds.push({ mainId: pGroup.id, subId: group.id, title: pGroup.title, subTitle: ""});
+
                                 chrome.tabGroups.update(group.id, { title: `sub-${pGroup.title}` });
                             }
                         })
@@ -83,3 +136,41 @@ function tabCreated(group) {
         })
     });
 }
+
+// function constructTabArrays() {
+
+//     // for iterating through the tabs
+//     tabIds.forEach(tabObj => {
+        
+
+//         if (groupDoesNotExist) {
+//             // create Tab Array Object
+//             addObject(getGroupObject(tabObj.groupId));
+//         }
+//         if (tabObj.groupId==current) {
+//             // if object with that group id exists?
+//             addObject(tabObj);
+//         }
+//         else if (tabObj.groupId!=null) {
+//             //
+//             tabObj.groupId = new tabArray;
+//         }
+//     })
+
+//     if (groupId) {
+//         addObject({})
+//     }
+// }
+
+// //class for creating instances of tab arrays
+
+// class tabArrays {
+
+//     tabArray = []
+
+//     addObject(tabObject) {
+//         this.tabArray.push(tabObject)
+//     }
+// }
+
+//[ {Groupid: 1231231312, groupName: "whatever"}, {tabName: "soemthing", url: "soemthing", id: ""}, {groupName: "whatever", id: 321321123, tabGroups: [{tabname: "whatever"}, {tabName: "son"}]}]
